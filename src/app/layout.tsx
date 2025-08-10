c458f4e92d9cee889ed84f7fb1fbd3044bce259e
RahulKaushal09 <rahul82rahul2002@gmail.com>
2025-08-10 12:49:39 +0530

    clarity integrated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,8 @@ export const metadata = {
   title: 'Home | SyncTrip',
   description: 'Discover and join trips near you.',
 };
+import Script from 'next/script';
+
 // import generateSitemap from '@/temp/generateStaticSitemap';
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
@@ -35,6 +37,17 @@ export default async function RootLayout({ children }: { children: React.ReactNo
   return (
     <html lang="en">
       <body>
+      <Script
+          id="clarity-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "ss9aac6yj2");`,
+          }}
+        />
         <link rel="icon" href="/favicon.ico" sizes="any" />
         <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
