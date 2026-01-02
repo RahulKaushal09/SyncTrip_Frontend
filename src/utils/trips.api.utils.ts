25581dc33e1343f50616a3195d924fcd13e32713
RKaushal09 <rahul.kaushal@edurev.in>
2026-01-02 15:28:20 +0530

    changes

--- src/utils/trips.api.utils.ts
+++ src/utils/trips.api.utils.ts
@@ -22,15 +22,12 @@ export class TripsApiService {
         return allTripsData;
     }
     static async fetchAllHostedTrips(): Promise<HostedTrip[]> {
-        console.log('Fetching all hosted trips from API...');
         const allTripsResponse = await fetch(`${API_CONFIG.BACKEND_BASE_URL}/api/hostedTrips`,{
             cache: 'no-store',
   next: { revalidate: 0 },
             method: 'GET',
             headers: { 'Content-Type': 'application/json' },
         });
-        console.log('Response status:', allTripsResponse.status);
-        console.log('Response :', allTripsResponse);
         if (allTripsResponse.status === 404) {
             throw new Error('No trips found. Please check back later.');
             return [];
