3f7283224a29de9fc8c4c5ff61f99c87e7f78163
RKaushal09 <rahul.kaushal@edurev.in>
2026-01-02 15:28:27 +0530

    changes

--- src/app/hostedTrips/page.tsx
+++ src/app/hostedTrips/page.tsx
@@ -89,9 +89,7 @@ export async function generateMetadata(): Promise<Metadata> {
 
 const HostedTripsPage = async () => {
   const res = await TripsApiService.fetchAllHostedTrips();
-  console.log('Fetched hosted trips:', res);
   const trips = res;
-  console.log('Hosted Trips:', trips);
 
   return (
     <div className="trips-container">
