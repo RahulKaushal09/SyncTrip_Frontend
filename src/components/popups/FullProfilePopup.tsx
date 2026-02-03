c257047cfc678ed906490ea7cf5d32d38f00086a
RahulKaushal09 <rahul82rahul2002@gmail.com>
2026-02-03 18:30:41 +0530

    Bug wihtout Image profile

--- src/components/popups/FullProfilePopup.tsx
+++ src/components/popups/FullProfilePopup.tsx
@@ -205,7 +205,7 @@ export default function FullProfilePopup({ user, onClose, onProfileComplete }: F
             if (!form.travelGoal) return "Please select your travel goal";
             if (form.languages.length === 0) return "Please select minimum one language you understand and speak";
         } else if (step === 2) {
-            const anyValidImage = form.profilePicture || (user.profile_picture && user.profile_picture.length > 0) || (user.profile_picture && user.profile_picture?.map(pic => pic != "").includes(true));
+            const anyValidImage = form.profilePicture ||  (user.profile_picture && user.profile_picture?.map(pic => pic != "").includes(true));
             if (!anyValidImage) return "Profile picture is required";
         }
         else if (step === 3) {
